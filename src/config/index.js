37343970451096cb025d4dc2735610214f202f34
Zoie Cheng <greenmoonshisu@gmail.com>
2022-12-09 17:07:37 +0800

    update config

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,5 @@
 const config = {
-  BASE_URL: process.env.REACT_APP_API_URL,
+    BASE_URL: process.env.REACT_APP_API_URL,
 };
-
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  //config.BASE_URL = process.env.REACT_APP_API_URL;
-  config.BASE_URL = 'https://stackoverflow-clone-backend.herokuapp.com';
-}
 
 export default config;
